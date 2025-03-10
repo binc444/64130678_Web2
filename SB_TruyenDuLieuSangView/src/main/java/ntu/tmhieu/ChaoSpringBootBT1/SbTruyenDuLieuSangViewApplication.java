1ab579f02d49ca56b77ae1f7cf64933df38d5bca
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2025-03-10 15:45:32 +0700

    Tạo project

---
+++ SB_TruyenDuLieuSangView/src/main/java/ntu/tmhieu/ChaoSpringBootBT1/SbTruyenDuLieuSangViewApplication.java
@@ -0,0 +1,13 @@
+package ntu.tmhieu.ChaoSpringBootBT1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTruyenDuLieuSangViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTruyenDuLieuSangViewApplication.class, args);
+	}
+
+}
---
+++ SB_TruyenDuLieuSangView/src/test/java/ntu/tmhieu/ChaoSpringBootBT1/SbTruyenDuLieuSangViewApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.tmhieu.ChaoSpringBootBT1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTruyenDuLieuSangViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
