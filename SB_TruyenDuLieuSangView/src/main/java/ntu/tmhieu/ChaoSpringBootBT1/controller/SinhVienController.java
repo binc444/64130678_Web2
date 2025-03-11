97ccf866648a6d032febf1b55f2adcb0d7c48553
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2025-03-11 10:30:30 +0700

    Cập nhật controller(sử dụng ArrayList để thêm nhiều sinh viên)

--- SB_TruyenDuLieuSangView/src/main/java/ntu/tmhieu/ChaoSpringBootBT1/controller/SinhVienController.java
+++ SB_TruyenDuLieuSangView/src/main/java/ntu/tmhieu/ChaoSpringBootBT1/controller/SinhVienController.java
@@ -1,5 +1,8 @@
 package ntu.tmhieu.ChaoSpringBootBT1.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +17,22 @@ public class SinhVienController {
         SinhVien sinhVien = new SinhVien("64130678", "Trần Minh Hiếu", 2004, "Nam");
         model.addAttribute("sinhVien", sinhVien);
         return "sinhVienView";
+    }
+    
+ // Lấy danh sách sinh viên
+    private List<SinhVien> getData() {
+        List<SinhVien> dsSinhVien = new ArrayList<>();
+        dsSinhVien.add(new SinhVien("64123456", "Hiếu", 2004, "Nam"));
+        dsSinhVien.add(new SinhVien("64456789", "Hưng", 2004, "Nam"));
+        dsSinhVien.add(new SinhVien("64789101", "Hoàng", 2004, "Nam"));
+        return dsSinhVien;
+    }
+
+    // Action hiển thị danh sách sinh viên
+    @GetMapping("/danhsachsinhvien")
+    public String danhSachSinhVien(Model model) {
+        List<SinhVien> dsSinhVien = getData();
+        model.addAttribute("dsSV", dsSinhVien);
+        return "dsSinhVienView";
     }
 }
