e949a743838be04ad91ac4580ca06164a8b3b2a5
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2025-03-17 22:11:04 +0700

    Hoàn thành bài tập

--- SB_TruyenDuLieuSangView/src/main/java/ntu/tmhieu/ChaoSpringBootBT1/controller/LoginController.java
+++ SB_TruyenDuLieuSangView/src/main/java/ntu/tmhieu/ChaoSpringBootBT1/controller/LoginController.java
@@ -1,5 +1,31 @@
 package ntu.tmhieu.ChaoSpringBootBT1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller  
 public class LoginController {
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(HttpServletRequest request, Model model) {
+        String id = request.getParameter("id");
+        String pw = request.getParameter("password");
+
+        // Kiểm tra tài khoản
+        if ("admin".equals(id) && "12345".equals(pw)) {
+            return "redirect:/danhsachsinhvien"; 
+        }
+
+        model.addAttribute("error", "Sai tài khoản hoặc mật khẩu"); 
+        return "login"; 
+    }
 }
