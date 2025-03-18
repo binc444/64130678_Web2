071eca2a22407e20d3cd57a3c93d00bdcf964c7f
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2025-03-18 10:45:13 +0700

    create project

---
+++ SB_Fragment/src/main/java/ntu/tmhieu/FragmentBT/SbFragmentApplication.java
@@ -0,0 +1,13 @@
+package ntu.tmhieu.FragmentBT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFragmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFragmentApplication.class, args);
+	}
+
+}
---
+++ SB_Fragment/src/test/java/ntu/tmhieu/FragmentBT/SbFragmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.tmhieu.FragmentBT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbFragmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
