2099ad3dddf1fe22046fa143999e9fb064af4f86
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2025-04-01 09:21:02 +0700

    Tạo layout chung

--- tranminhhieu_hieuBlog/src/main/java/thiGK/ntu64130678/controller/HomeController.java
+++ tranminhhieu_hieuBlog/src/main/java/thiGK/ntu64130678/controller/HomeController.java
@@ -1,5 +1,14 @@
 package thiGK.ntu64130678.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+	@GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("userName", "Admin");
+        return "frontEndViews/index";
+    }
 }
