18de196c1e002f369c90647b74460e15590802c1
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2025-04-01 09:04:14 +0700

    tạo project thi giữa kì

---
+++ tranminhhieu_hieuBlog/src/main/java/thiGK/ntu64130678/TranminhhieuHieuBlogApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130678;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranminhhieuHieuBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranminhhieuHieuBlogApplication.class, args);
+	}
+
+}
---
+++ tranminhhieu_hieuBlog/src/test/java/thiGK/ntu64130678/TranminhhieuHieuBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130678;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranminhhieuHieuBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
