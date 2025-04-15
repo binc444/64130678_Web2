54c354547d3a94721d69645fe06b7ded317dc977
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2025-04-15 08:55:21 +0700

    Tạo project làm lại bài giữa kì

---
+++ tranminhhieu_hieuBlog2/src/main/java/thiGK/ntu64130678/TranminhhieuHieuBlog2Application.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130678;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranminhhieuHieuBlog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranminhhieuHieuBlog2Application.class, args);
+	}
+
+}
